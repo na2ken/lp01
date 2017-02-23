13cdb76ca7b29eedb3886f7aeadc6d6ad22e141c
kenji_natsumoto <na2ken@users.noreply.github.com>
2017-02-23 17:18:43 +0900

    chage code.

--- page-lp01.php
+++ page-lp01.php
@@ -2,7 +2,7 @@
 /* Template Name: ランディングページ用テンプレート01 */
 ?>
 <!DOCTYPE html>
-<html lang="ja" id="pageTop">
+<html lang="ja" id="pageTop page-lp01">
 <head>
     <meta charset="UTF-8">
     <meta name="robots" content="noindex, nofollow" />
@@ -16,7 +16,6 @@
 </head>
 
 <body class="lp lp01">
-
   <div class="l-cover-justvertical verticalPadding-t-0 verticalPadding-b-0">
       <div class="container-fluid" id="section00">
           <div class="row no-gutter">
@@ -31,32 +30,34 @@
             <img src="http://bp-media.iaowd.com/wp-content/themes/bootpress-media/img/noimageLp.png" alt="<?php the_title(); ?>" class="img-responsive">
           </figure>
       <?php endif; ?>
-      <div class="parent textLyer-box">
-        <div class="lyer textLyer-body">
+
+      <div class="flexBox__area2">
+        <div class="flexBox2">
+          <div class="container">
+              <div class="row">
           <?php if(have_posts()): while(have_posts()): the_post(); ?>
-          <h1 class="text-center wht-text"><?php the_title(); ?></h1>
-  </div>
-  </div>
+          <h1 class="textSize-lg text-center wht-text"><?php the_title(); ?></h1>
+        </div>
+       </div>
+     </div>
 
+   </div><!-- /.verticalMargin-t-0 -->
 
-              </div><!-- /.col-sm-12 -->
 
           </div><!-- /.row -->
-      </div>
+      </div><!-- /.container-fluid -->
+
+
   </div><!-- /.l-cover -->
-
-
-
 
     <article class="gryColorTone2nd">
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 gryColor">
-                <?php remove_filter ('the_content', 'wpautop'); ?>
                 <?php the_content(); ?>
                 <?php endwhile; endif; ?>
                 </div>
-    	</div><!-- /.row -->
+    	      </div><!-- /.row -->
         </div><!-- /.container -->
     </article>
     <footer class="l-footer">
@@ -71,5 +72,6 @@
                        </div>
                  </div>
            </div>
+    </footer>
 </body>
 <html>
